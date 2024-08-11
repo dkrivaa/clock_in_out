b08e7deac6b770490c0ca3d7fb075cdc00acf8f9
dkrivaa <dannykrivaa@hotmail.com>
2024-08-11 13:58:54 +0300

    initial commit

--- site_pages/login.py
+++ site_pages/login.py
@@ -4,7 +4,7 @@ import streamlit as st
 login = st.text_input('Enter Password', type='password', placeholder='Enter Password')
 
 if login != '':
-    if login == 'test':
+    if login == st.secrets['PASSWORD']:
         st.switch_page('site_pages/home.py')
 
     else:
